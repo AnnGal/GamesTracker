21171bfe7f6a98df5447c1afa7567cd1f7d94b6a
AnnGal <galyamova.anya@gmail.com>
2020-06-12 01:44:53 +0300

    feat: add layouts and GameCard fields

--- app/src/main/java/com/example/android/gamescalendartracker/ui/viewcard/GameCard.java
+++ app/src/main/java/com/example/android/gamescalendartracker/ui/viewcard/GameCard.java
@@ -1,5 +1,50 @@
 package com.example.android.gamescalendartracker.ui.viewcard;
 
-public class GameCard {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class GameCard {
+    private int cover;  // img id
+    private String title;
+    private String developer; //enum?
+    private String publisher; // month YYYY
+    private Date release;
+    private ArrayList<String> platforms = new ArrayList<>();
+
+
+    public GameCard(int cover, String title, String developer, String publisher, Date release, ArrayList<String> platforms) {
+        //TODO: if not specified, then set 'Unknown'
+        this.cover = cover;
+        this.title = title;
+        this.developer = developer;
+        this.publisher = publisher;
+        //TODO: set date format "month YYYY"
+        this.release = release;
+        this.platforms = platforms;
+    }
+
+
+    public int getCover() {
+        return cover;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Date getRelease() {
+        return release;
+    }
+
+    public ArrayList<String> getPlatforms() {
+        return platforms;
+    }
 }
